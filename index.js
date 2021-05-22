1c29ff04449668447fcd76513d71d82f175d9d23
hproctor021 <hproctor021@gmail.com>
2021-05-21 22:21:01 -0500

    completed autocomplete and started show page

--- index.js
+++ index.js
@@ -31,19 +31,90 @@ const resultsWrapper = document.querySelector('.results')
 
 const onInput = async e => {
     const movies = await fetchData(e.target.value)
-    for( let movie of movies ){
-        const div = document.createElement('div')
+    
+    if( !movies.length ){
+        dropdown.classList.remove('is-active')
+        return
+    }
 
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
+    resultsWrapper.innerHTML = ''
+    dropdown.classList.add('is-active')
+    for( let movie of movies ){
+        const option = document.createElement('a')
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title}
         `
         // use backticks when creating a multi-line string
 
-        document.querySelector('#target').appendChild(div)
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active')
+            input.value = movie.Title
+            onMovieSelect(movie)
+        })
+
+        resultsWrapper.appendChild(option)
     }
 };
 
 input.addEventListener('input', debounce(onInput, 750))
 // add the callback function in as a second parameter rather than 
 // writing out the function, so on input we call on the onInput fxn
+
+document.addEventListener('click', e => {
+    if( !root.contains(e.target) ){
+        dropdown.classList.remove('is-active')
+    }
+})
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '7b74a652',
+            i: movie.imdbID
+        }
+    })
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure calss="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `
+}
