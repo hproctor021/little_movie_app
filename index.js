29767c103b4fc6602a6846b35cd297eb9af3f96d
hproctor021 <hproctor021@gmail.com>
2021-05-21 21:12:02 -0500

    added dropdown logic to fill movie search results

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const fetchData = async (searchTerm) => {
+const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '7b74a652',
             s: searchTerm
         }
     })
-    console.log(response.data)
+
+    if( response.data.Error ){
+        return []
+    }
+    // if there are no results, (no match found) just return an empty array
+    return response.data.Search
 };
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`
 
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
-const onInput = (e) => {
-    fetchData(e.target.value)    
+
+const onInput = async e => {
+    const movies = await fetchData(e.target.value)
+    for( let movie of movies ){
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `
+        // use backticks when creating a multi-line string
+
+        document.querySelector('#target').appendChild(div)
+    }
 };
 
 input.addEventListener('input', debounce(onInput, 750))
