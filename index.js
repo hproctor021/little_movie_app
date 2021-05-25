41c818e56783480d3d0ded69358e23ce69947817
hproctor021 <hproctor021@gmail.com>
2021-05-24 19:15:55 -0500

    little fixes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autocompleteConfig = {
+    // this function is for rendering the autocomplete options in the drop down menu below search bar
     renderOption(movie){
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
         return `
         <img src="${imgSrc}" />
         ${movie.Title} (${movie.Year})
     `
-    // use backticks when creating a multi-line string
-    },
-
-    onOptionSelect(movie){
-        onMovieSelect(movie)
+    // use backticks when creating a multi-line string to return
     },
 
     inputValue(movie){
@@ -29,25 +24,107 @@ createAutoComplete({
         if( response.data.Error ){
             return []
         }
-        // if there are no results, (no match found) just return an empty array
+        // if there are no results, (no match found) return an empty array, to display nothing in the results menu
         return response.data.Search
+    }
+}
+
+
+createAutoComplete({
+    ...autocompleteConfig,
+    // by using ... everything in the autocompleteConfig fxn will be copied
+    // and placed into this fxn
+    root: document.querySelector('#left-autocomplete'),
+
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left')
+        // add second argument to let it know where to put the info
+    }
+})
+createAutoComplete({
+    ...autocompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right')
     }
 })
 
-const onMovieSelect = async movie => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
+    // once a movie is clicked on, we get the movie and display its info on the correct side
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '7b74a652',
             i: movie.imdbID
         }
     })
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
+    // send the response from our get request to the movieTemplate fxn
+
+    if( side === 'left' ){
+        leftMovie = response.data
+    } else {
+        rightMovie = response.data
+    }
+    
+
+    const runComparison = () => {
+        leftSideStats = document.querySelectorAll('#left-summary .notification')
+        rightSideStats = document.querySelectorAll('#right-summary .notification')
+
+        leftSideStats.forEach((leftStat, index) => {
+            const rightStat = rightSideStats[index]
+
+            const leftSideValue = leftStat.dataset.value
+            const rightSideValue = rightStat.dataset.value
+            // in the JSX below we used data-value, so that's how we will access it
+
+            if(rightSideValue > leftSideValue ){
+                leftStat.classList.remove('is-primary')
+                leftStat.classList.add('is-warning')
+            } else {
+                rightStat.classList.remove('is-primary')
+                rightStat.classList.add('is-warning')
+            }
+            // this portion determines if the block should be green or yellow based on comparison values of each movie
+        })
+    }
+
+    if( leftMovie && rightMovie ){
+        runComparison()
+        //run comparison fxn once both movies have been selected
+    }
+
 }
 
 const movieTemplate = (movieDetail) => {
+
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''))
+    const metascore = parseInt(movieDetail.Metascore)
+    const imdbRating = parseFloat(movieDetail.imdbRating)
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+    const value = parseInt(word)
+
+        if( isNaN(value) ){
+            return prev
+            // if the value we are currently looking at is NOT a number, do nothing with it & keep looking for numbers
+        } else {
+            return prev + value
+            // if it is a number add it to our tracker
+        }
+
+    }, 0)
+    // passing 0 as a second argument will be a place to keep count, referenced at prev above
+
+
     return `
         <article class="media">
-            <figure calss="media-left">
+            <figure class="media-left">
                 <p class="image">
                     <img src="${movieDetail.Poster}" />
                 </p>
@@ -60,23 +137,23 @@ const movieTemplate = (movieDetail) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${awards} class="notification is-primary">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${dollars} class="notification is-primary">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${metascore} class="notification is-primary">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbRating} class="notification is-primary">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification is-primary">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
--- utils.js
+++ utils.js
@@ -6,7 +6,10 @@ const debounce = (func, delay = 1000) => {
         }
         timeoutId = setTimeout(() => {
             func.apply(null, args)
-            // .apply looks at given args if more than 1 & applies individually
+            // .apply looks at given args if there is more than 1, it applies them individually
         }, delay)
     }
 }
+// this function is to monitor if the user is typing or not in the searach bar, 
+// if typing, then will wait 1 second after no typing to run the autocomplete functions
+// and populate the results
